800e6b4aea6ae0c260204b7339657a305a5ed7d4
William Chargin <wchargin@gmail.com>
2015-10-02 18:11:23 -0700

    Add getConditionCode, formatConditionCode

--- test/core/lc3_spec.js
+++ test/core/lc3_spec.js
@@ -1,41 +1,73 @@
 import {expect} from 'chai';
 
-import LC3 from '../../src/core/lc3';
+import LC3, {getConditionCode, formatConditionCode} from '../../src/core/lc3';
 import Constants from '../../src/core/constants';
 
 describe('LC3', () => {
 
-    const lc3 = LC3();
+    describe("the LC3 constructor", () => {
 
-    it("creates some sort of thing", () => {
-        expect(lc3).to.be.ok;
+        const lc3 = LC3();
+
+        it("creates some sort of thing", () => {
+            expect(lc3).to.be.ok;
+        });
+
+        it("has a memory buffer of the right length", () => {
+            const memory = lc3.get("memory");
+            expect(memory.size).to.equal(Constants.MEMORY_SIZE);
+        });
+
+        it("has the right number of registers", () => {
+            const registers = lc3.get("registers");
+            expect(registers.size).to.equal(
+                Constants.REGISTER_NAMES.get("all").size);
+        });
+
+        it("has the program counter set to 0x3000", () => {
+            const registers = lc3.get("registers");
+            const pc = registers.get("PC");
+            expect(pc).to.equal(0x3000);
+        });
+
+        it("has a symbol table", () => {
+            const symbolTable = lc3.get("symbolTable");
+            expect(symbolTable).to.be.ok;
+        });
+
+        it("starts with an empty console buffer", () => {
+            const consoleBuffer = lc3.get("consoleBuffer");
+            expect(consoleBuffer).to.equal("");
+        });
+
     });
 
-    it("has a memory buffer of the right length", () => {
-        const memory = lc3.get("memory");
-        expect(memory.size).to.equal(Constants.MEMORY_SIZE);
+    describe('getConditionCode', () => {
+        const test = (psr, expected) => () => {
+            expect(getConditionCode(psr)).to.equal(expected);
+        };
+
+        it("handles the negative case", test(0x8004, -1));
+        it("handles the zero case", test(0x8002, 0));
+        it("handles the positive case", test(0x8001, 1));
+
+        it("fails when none of the bits is set", test(0x8000, null));
+        it("fails when two of the bits are set", test(0x8003, null));
+        it("fails when all the bits are set", test(0x8007, null));
     });
 
-    it("has the right number of registers", () => {
-        const registers = lc3.get("registers");
-        expect(registers.size).to.equal(
-            Constants.REGISTER_NAMES.get("all").size);
-    });
+    describe('formatConditionCode', () => {
+        const test = (psr, expected) => () => {
+            expect(formatConditionCode(psr)).to.equal(expected);
+        };
 
-    it("has the program counter set to 0x3000", () => {
-        const registers = lc3.get("registers");
-        const pc = registers.get("PC");
-        expect(pc).to.equal(0x3000);
-    });
+        it("handles the negative case", test(0x8004, "N"));
+        it("handles the zero case", test(0x8002, "Z"));
+        it("handles the positive case", test(0x8001, "P"));
 
-    it("has a symbol table", () => {
-        const symbolTable = lc3.get("symbolTable");
-        expect(symbolTable).to.be.ok;
-    });
-
-    it("starts with an empty console buffer", () => {
-        const consoleBuffer = lc3.get("consoleBuffer");
-        expect(consoleBuffer).to.equal("");
+        it("fails when none of the bits is set", test(0x8000, "Invalid"));
+        it("fails when two of the bits are set", test(0x8003, "Invalid"));
+        it("fails when all the bits are set", test(0x8007, "Invalid"));
     });
 
 });
