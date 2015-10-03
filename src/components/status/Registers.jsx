5344cd8bfb865b14781f61fd71c8b3fa59c260dd
William Chargin <wchargin@gmail.com>
2015-10-02 18:17:57 -0700

    Show condition code in registers panel

--- src/components/status/Registers.jsx
+++ src/components/status/Registers.jsx
@@ -2,32 +2,46 @@ import React, {PropTypes, Component} from 'react';
 import {Map} from 'immutable';
 
 import {REGISTER_NAMES} from '../../core/constants';
+import {formatConditionCode} from '../../core/lc3';
 
 import {Col, Panel} from 'react-bootstrap';
 
 import NumericValue from '../NumericValue';
+
+class Register extends Component {
+
+    render() {
+        return <Col xs={6} sm={3}>
+            <b>{this.props.name}:</b>
+            {" "}
+            {this.props.children}
+        </Col>;
+    }
+
+}
 
 export default class Registers extends Component {
 
     render() {
         const {registers} = this.props;
 
-        const makeRegister = (name, signed) => <Col xs={6} sm={3}>
-            <b>{name}:</b>
-            {" "}
-            <NumericValue
-                value={registers[name]}
-                signed={signed}
-                id={name}
-            />
-        </Col>;
+        const makeNumericRegister = (name, signed) =>
+            <Register name={name}>
+                <NumericValue
+                    value={registers[name]}
+                    signed={signed}
+                    id={name}
+                />
+            </Register>;
 
         return <Panel header="Registers">
             {REGISTER_NAMES.get("standard").map(name =>
-                makeRegister(name, true)).toJS()}
+                makeNumericRegister(name, true)).toJS()}
             {REGISTER_NAMES.get("special").map(name =>
-                makeRegister(name, true)).toJS()}
-            {/* TODO(william): add condition codes */}
+                makeNumericRegister(name, true)).toJS()}
+            <Register name="CC">
+                {formatConditionCode(registers["PSR"])}
+            </Register>
         </Panel>;
     }
 
