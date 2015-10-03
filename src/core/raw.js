e234c1e958edaef9bfc76024d2d1a244a85fdcda
William Chargin <wchargin@gmail.com>
2015-10-02 22:02:10 -0700

    Add raw hex/binary parsing

---
+++ src/core/raw.js
@@ -0,0 +1,110 @@
+/*
+ * Parse raw binary or hexadecimal data.
+ */
+
+export default function parseRaw(data) {
+    const contents = removeComments(data);
+
+    const maybeInputType = guessInputType(contents);
+    if (maybeInputType.status === "error") {
+        return maybeInputType;
+    }
+    const inputType = maybeInputType.type;
+
+    const answer = extractData(contents, inputType);
+    if (answer.status === "error") {
+        return answer;
+    }
+
+    return {
+        symbolTable: {},
+        ...answer,
+        status: "success",
+    };
+}
+
+/*
+ * Remove comments, preserving empty lines (for error reporting, etc.).
+ */
+function removeComments(text) {
+    return (text || "").split("\n").map(function(line) {
+        // Split on the comment character, and just take the first part
+        // (i.e., everything before the comment starts).
+        return line.split(";")[0];
+    }).join("\n");
+};
+
+/*
+ * Guess whether this is binary or hex, or fail if neither matches.
+ * Returns either { error: <error message string> }
+ * or { type: "hex" | "binary" }.
+ */
+function guessInputType(input) {
+    // Make sure the input doesn't have invalid characters.
+    const lines = input.split("\n");
+    for (let i = 0; i < lines.length; i++) {
+        const line = lines[i];
+        const match = line.match(/[^\s0-9A-Fa-f]/);
+        if (!match) {
+            continue;
+        }
+
+        const badCharacter = match[0];
+        const message =
+            `Invalid character "${badCharacter}" at line ${i + 1}`;
+        return {
+            status: "error",
+            message,
+        };
+    }
+
+    const justData = input.replace(/\s/g, "");
+    const isHex = !!justData.match(/[^01]/);
+
+    const shouldDivide = isHex ? 4 : 16;
+    if (justData.length % shouldDivide !== 0) {
+        const characterNoun = justData.length === 1 ?
+            "character" : "characters";
+        const dataType = isHex ? "hexadecimal" : "binary";
+        const message =
+            `Found a total of ${justData.length} ${characterNoun}, ` +
+            `but expected length to be a multiple of ${shouldDivide} ` +
+            `for ${dataType} data.`;
+        return {
+            status: "error",
+            message,
+        };
+    }
+
+    return {
+        status: "success",
+        type: isHex ? "hex" : "binary",
+    };
+};
+
+function extractData(lines, inputType) {
+    const justData = lines.replace(/\s/g, "");
+
+    const charsPerWord = inputType === "hex" ? 4 : 16;
+    const words = new Array(justData.length / charsPerWord);
+    if (words.length === 0) {
+        const message = "Your raw data is empty! " +
+            "You need to at least have an origin (.ORIG) address.";
+        return {
+            status: "error",
+            message,
+        };
+    }
+
+    for (let i = 0; i < words.length; i++) {
+        const start = charsPerWord * i;
+        words[i] = justData.substr(start, charsPerWord);
+    }
+
+    const base = inputType === "hex" ? 16 : 2;
+    const machineCode = words.map(word => parseInt(word, base));
+    return {
+        orig: machineCode[0],
+        machineCode: machineCode.slice(1),
+    };
+};
---
+++ test/core/raw_spec.js
@@ -0,0 +1,61 @@
+import {expect} from 'chai';
+
+import {fromJS} from 'immutable';
+
+import parseRaw from '../../src/core/raw';
+
+describe('parseRaw', () => {
+    const success = (input, orig, data) => () => {
+        const output = parseRaw(input);
+        expect(output.status).to.equal("success");
+        expect(output.orig).to.equal(orig);
+        expect(fromJS(output.machineCode)).to.equal(fromJS(data));
+        expect(fromJS(output.symbolTable)).to.equal(fromJS({}));
+    };
+
+    const failure = (input, message) => () => {
+        const output = parseRaw(input);
+        expect(output.status).to.equal("error");
+        if (message) {
+            expect(output.message).to.contain(message);
+        }
+    }
+
+    it("should fail when given empty data", failure("", "empty"));
+    it("should fail when given binary data of a bad length",
+        failure("0000", "length"));
+    it("should fail when given hex data of a bad length",
+        failure("abc", "length"));
+    it("should fail when given non-hex characters",
+        failure("efg", "character"));
+
+    it("should succeed when only given a binary .ORIG",
+        success("0011 0000 0000 0000", 0x3000, []));
+    it("should succeed when only given a hex .ORIG",
+        success("3000", 0x3000, []));
+
+    it("should parse a few words of binary",
+        success("0111 0000 0000 0000 \n" +
+                "0101 1010 1111 0000 \n" +
+                "1000 0100 0010 0001",
+            0x7000, [0x5AF0, 0x8421]));
+
+    it("should parse a few words of hex",
+        success("4000 1a2b CAFE bEeF 0000",
+            0x4000, [0x1A2B, 0xCAFE, 0xBEEF, 0x0000]));
+
+    it("should parse binary with comments",
+        success("0111 0000 0000 0000 ; this is the .ORIG\n" +
+                "0101 1010 1111 0000 ; random junk\n" +
+                "; this is a comment-only line\n" +
+                "   ; comment-and-whitespace-only line\n" +
+                "1000 0100 0010 0001",
+            0x7000, [0x5AF0, 0x8421]));
+
+    it("should parse hex with comments",
+        success("9001 ; .ORIG\n" +
+                "1000 2000 3000 4000 ; data section\n" +
+                "; that's all for this program\n" +
+                "4321  ; oh yeah, except for that",
+            0x9001, [0x1000, 0x2000, 0x3000, 0x4000, 0x4321]));
+});
