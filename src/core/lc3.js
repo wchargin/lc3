c2528710b0abaa1db366299ab221ef7d1052497d
William Chargin <wchargin@gmail.com>
2015-10-09 12:56:57 -0700

    Implement the LC3 interpreter

--- src/core/lc3.js
+++ src/core/lc3.js
@@ -3,6 +3,7 @@ import {List, Map, Record, fromJS} from 'immutable';
 import Constants from './constants';
 import RegisterSet from './register_set';
 import Utils from './utils';
+import decode from './instructions';
 
 /*
  * Create the initial memory for the LC3.
@@ -69,6 +70,168 @@ export default class LC3 extends Record({
         } else {
             return Utils.toHexString(address);
         }
+    }
+
+    _cycle() {
+        const instructionValue = this.memory.get(this.registers.pc);
+        return this
+            .updateIn(["registers", "pc"], x => x + 1)
+            .setIn(["registers", "ir"], instructionValue)
+            ._execute(instructionValue);
+    }
+
+    step(n = 1) {
+        return this.withMutations(lc3 => {
+            for (let i = 0; i < n; i++) {
+                lc3._cycle();
+            }
+            return lc3;
+        });
+    }
+
+    /*
+     * Execute the given instruction, provided as a (binary) number.
+     * This does *not* correspond to the full instruction cycle!
+     * In particular, it does not include the "fetch" phase,
+     * so the instruction register is not modified,
+     * and the PC will only be modified if the instruction is a BR, JMP, etc.
+     */
+    _execute(instructionValue) {
+        const instruction = decode(instructionValue);
+        const address = this._evaluateAddress(instruction);
+
+        const opcode = instruction.get("opcode");
+        switch (opcode) {
+            case 0b0001:  // ADD
+                return this._executeBinop(instruction, (x, y) => x + y);
+
+            case 0b0101:  // AND
+                return this._executeBinop(instruction, (x, y) => x & y);
+
+            case 0b0000:  // BR
+                const cc = Utils.getConditionCode(this.registers.psr);
+                const nzp = (cc < 0) ? "n" : (cc > 0) ? "p" : "z";
+                const shouldBranch = instruction.get(nzp);
+                if (shouldBranch) {
+                    return this.setIn(["registers", "pc"], address);
+                } else {
+                    return this;
+                }
+
+            case 0b1100:  // JMP, RET
+                return this.setIn(["registers", "pc"], address);
+
+            case 0b0100:  // JSR, JSRR
+                return this.update("registers", rs => rs
+                    .set("r7", rs.get("pc"))
+                    .set("pc", address));
+
+            case 0b0010:  // LD
+            case 0b0110:  // LDR
+                return this._setRegisterCC(
+                    instruction.get("dr"), this.memory.get(address));
+
+            case 0b1010:  // LDI
+                return this._setRegisterCC(
+                    instruction.get("dr"),
+                    this.memory.get(this.memory.get(address)));
+
+            case 0b1110:  // LEA
+                return this._setRegisterCC(instruction.get("dr"), address);
+
+            case 0b1001:  // NOT
+                return this._setRegisterCC(
+                    instruction.get("dr"), Utils.toUint16(
+                        ~this.registers.getNumeric(instruction.get("sr"))));
+
+            case 0b1000:  // RTI
+                throw new Error("RTI not yet implemented");
+
+            case 0b0011:  // ST
+            case 0b0111:  // STR
+                return this.setIn(["memory", address],
+                    this.registers.getNumeric(instruction.get("sr")));
+
+            case 0b1011:  // STI
+                return this.setIn(["memory", this.memory.get(address)],
+                    this.registers.getNumeric(instruction.get("sr")));
+
+            case 0b1111:  // TRAP
+                return this.update("registers", rs => rs
+                    .setNumeric(7, rs.pc)
+                    .set("pc", this.memory.get(address)));
+
+            case 0b1101:  // RSRV
+                throw new Error(
+                    "Reserved instruction not yet handled " +
+                    "(this should halt the machine and set a status flag)");
+
+            default:
+                throw new Error(`Unknown opcode: ${opcode}`);
+        }
+    }
+
+    _evaluateAddress(instruction) {
+        const mode = instruction.get("mode");
+        switch (mode) {
+            case "none":
+                return null;
+            case "pcOffset":
+                return Utils.toUint16(
+                    this.registers.pc + instruction.get("offset"));
+            case "baseOffset":
+                const baseR = instruction.get("baseR");
+                const offset = instruction.get("offset");
+                return Utils.toUint16(
+                    this.registers.getNumeric(baseR) + offset);
+            case "trap":
+                return instruction.get("trapVector");
+            default:
+                throw new Error(`unknown addressing mode: ${mode}`);
+        }
+    }
+
+    _fetchOperand(address) {
+        if (address === null) {
+            return null;
+        } else {
+            return this.memory.get(address);
+        }
+    }
+
+    _executeBinop(instruction, computer) {
+        const source1 = this.registers.getNumeric(instruction.get("sr1"));
+
+        const source2 = (() => {
+            const mode = instruction.get("arithmeticMode");
+            switch (mode) {
+                case "immediate":
+                    return instruction.get("immediateField");
+                case "register":
+                    return this.registers.getNumeric(instruction.get("sr2"));
+                default:
+                    throw new Error(`Unknown arithmetic mode: ${mode}`);
+            }
+        })();
+
+        const result = Utils.toUint16(computer(source1, source2));
+
+        return this._setRegisterCC(instruction.get("dr"), result);
+    }
+
+    _setRegisterCC(registerNumber, value) {
+        return this
+            .update("registers", rs => rs.setNumeric(registerNumber, value))
+            ._setConditionCode(value);
+    }
+
+    _setConditionCode(value) {
+        const sign = Math.sign(Utils.toInt16(value));
+        const cc =
+            sign < 0 ? 0x4 :
+            sign > 0 ? 0x1 :
+            0x2;
+        return this.updateIn(["registers", "psr"], psr => (psr & ~0b111) | cc);
     }
 
 }
