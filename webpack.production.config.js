b96f9688a83d7b3eb5de09936794d6d9c619ecb3
William Chargin <wchargin@gmail.com>
2015-10-11 10:14:14 -0700

    Add production build script and config

---
+++ webpack.production.config.js
@@ -0,0 +1,33 @@
+var webpack = require('webpack');
+
+module.exports = {
+    entry: [
+        './src/index.jsx',
+    ],
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel',
+            },
+            { test: /\.css$/, loader: 'style-loader!css-loader' },
+            { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
+            { test: /\.(woff|woff2)$/, loader:"url?prefix=font/&limit=5000" },
+            { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
+            { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" },
+        ],
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx'],
+    },
+    output: {
+        path: __dirname + '/dist',
+        publicPath: '/',
+        filename: 'bundle.js',
+    },
+    devServer: {
+        contentBase: './dist',
+        hot: false,
+    },
+};
