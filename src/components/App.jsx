eefcacc6bc0d2fba16e4cb3b0e8c78b438e6cb43
William Chargin <wchargin@gmail.com>
2015-10-11 10:15:02 -0700

    Remove loading bar because we're so fast

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,19 +2,10 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import {Col} from 'react-bootstrap';
 
-import LoadingMessage from './LoadingMessage';
 import MemoryView from './memory/MemoryView';
 import StatusView from './status/StatusView';
 
 export default class App extends Component {
-
-    componentDidMount() {
-        // Replace the static loading message
-        // with a dynamic React component,
-        // which will immediately fade out.
-        const target = document.getElementById('container-wait');
-        ReactDOM.render(<LoadingMessage />, target);
-    }
 
     render() {
         return <div className="container">
