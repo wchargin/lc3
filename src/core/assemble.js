11a58674760773c5cf2c051f8c68b5a60f9b5efc
William Chargin <wchargin@gmail.com>
2015-10-19 11:57:24 -0700

    Start adding assembly methods

---
+++ src/core/assemble.js
@@ -0,0 +1,35 @@
+import Utils from './utils';
+
+export function parseRegister(text) {
+    const match = text.match(/^[Rr]([0-7])$/);
+    if (match) {
+        return parseInt(match[1]);
+    } else {
+        throw new Error(`Invalid register specification: '${text}'`);
+    }
+}
+
+export function parseLiteral(text) {
+    const e = new Error(`Invalid numeric literal: '${text}'`);
+    const first = text.charAt(0);
+    if (first !== '#' && first.toLowerCase() !== 'x') {
+        throw e;
+    }
+
+    // Standard decimal or hexadecimal literal.
+    const isDecimal = (first === '#');
+    const negate = (text.charAt(1) === '-');
+    const toParse = isDecimal ?
+        text.substring(negate ? 2 : 1) :
+        (negate ? first + text.substring(2) : text);
+
+    const num = Utils.parseNumber(toParse);
+    if (isNaN(num)) {
+        throw e;
+    }
+    if (negate && num < 0) {
+        // No double negatives.
+        throw e;
+    }
+    return negate ? -num : num;
+}
---
+++ test/core/assemble_test.js
@@ -0,0 +1,43 @@
+import {describe, it} from 'mocha';
+import {expect} from 'chai';
+
+import assemble, * as assembleHelpers from '../../src/core/assemble';
+
+describe('assemble', () => {
+
+    const makeTesters = (fn) => ({
+        good: (...args) => (expected) => () =>
+            expect(fn.apply(null, args)).to.equal(expected),
+        bad: (...args) => (message) => () =>
+            expect(() => fn.apply(null, args)).to.throw(message),
+    });
+
+    describe("helper parseRegister", () => {
+        const {good, bad} = makeTesters(assembleHelpers.parseRegister);
+
+        it("works for R0", good("R0")(0));
+        it("works for r1", good("r1")(1));
+        it("works for R7", good("R7")(7));
+        it("fails for R8", bad("R8")());
+        it("fails for R-1", bad("R-1")());
+        it("fails for just R", bad("R")());
+        it("fails for 1R", bad("1R")());
+        it("fails for R12", bad("R12")());
+    });
+
+    describe("helper parseLiteral", () => {
+        const {good, bad} = makeTesters(assembleHelpers.parseLiteral);
+
+        it("parses #0", good("#0")(0));
+        it("parses #-1", good("#-1")(-1));
+        it("parses #1", good("#1")(1));
+        it("parses xF", good("xF")(15));
+        it("parses xf", good("xf")(15));
+        it("parses xf", good("xf")(15));
+        it("fails on xG", bad("xG")());
+        it("fails on #--1", bad("#--1")());
+        it("fails on START", bad("START")());
+        it("fails on x", bad("x")());
+    });
+
+});
