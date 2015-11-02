c3246e402637bda4178e6161457fc0c490b9caf3
William Chargin <wchargin@gmail.com>
2015-11-01 16:18:38 -0800

    Normalize newlines in input

--- src/components/console/Console.jsx
+++ src/components/console/Console.jsx
@@ -15,8 +15,26 @@ export default class Console extends Component {
         />;
     }
 
+    _normalizeKey(k) {
+        if (k !== 0x0A && k !== 0x0D) {
+            return k;
+        }
+
+        const newlineMode = this.props.console.get("newlineMode");
+        if (!["LF", "CR"].includes(newlineMode)) {
+            return k;
+        }
+
+        return ({
+            "LF": 0x0A,
+            "CR": 0x0D,
+        })[newlineMode];
+    }
+
     _handleKeyPress(e) {
-        this.props.onEnqueueStdin(String.fromCharCode(e.which));
+        const ascii = e.which;
+        const normalized = this._normalizeKey(ascii);
+        this.props.onEnqueueStdin(String.fromCharCode(normalized));
     }
 
 }
